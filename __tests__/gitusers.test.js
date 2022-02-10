4356d540a917429d827f0ec85193b572c33ca48e
Julius Franklin <jjfranklin88@outlook.com>
2022-02-10 15:36:23 -0500

    login/callback route with test passing

--- __tests__/gitusers.test.js
+++ __tests__/gitusers.test.js
@@ -3,6 +3,9 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const agent = request.agent(app);
+jest.mock('../lib/utils/github');
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -13,8 +16,22 @@ describe('backend routes', () => {
   });
 
   it('should redirect user to github page', async () => {
-    const res = await request(app).get('/api/v1/github/login');
+    const res = await agent.get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.GITHUB_REDIRECT_URI}&scope=user`);
+  });
+
+  it('should redirect user to the dashboard page', async () => {
+    const res = await agent.get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      avatar: expect.any(String),
+      email: 'not-real@example.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
